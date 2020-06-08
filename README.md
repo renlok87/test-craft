700fe32cda9025e0c4a9a48d68b54a4603d9beb6
renlok87 <renlok87@gmail.com>
2020-06-08 13:25:11 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # test-craft
+
+# Описание к тестовому
+https://drive.google.com/file/d/1Ene_kEMlkU4z98uXPxNHuhcOKLtBS50P/view?usp=sharing
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/app.tsx')
+  ],
+  resolve: {
+    root: [
+      path.resolve(__dirname, "src"),
+    ],
+    extensions: ['', '.js', '.jsx', '.css, ts, tsx']
+  },
+  output: {
+    path: path.join(__dirname, '/public/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    },{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+      {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  }
+};
